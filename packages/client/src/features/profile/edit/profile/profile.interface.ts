183c2801d9291007bc9c1a74ddf210f8ac1be8c9
Andrey Ivanov <andrey-tm203@mail.ru>
2023-03-29 18:31:14 +0300

    create edit profile page

--- packages/client/src/features/profile/edit/profile/profile.interface.ts
+++ packages/client/src/features/profile/edit/profile/profile.interface.ts
@@ -1,1 +1,7 @@
+import { IMenuData } from '@/features/profile/profile.interface';
 
+interface IMenuEditProfileData extends IMenuData {
+  name: string;
+}
+
+export default IMenuEditProfileData;
--- packages/client/src/features/profile/edit/profile/profile.tsx
+++ packages/client/src/features/profile/edit/profile/profile.tsx
@@ -1,36 +1,97 @@
-import React, { FC } from 'react';
+import React, { FC, FormEvent } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Avatar from '@/components/avatar/avatar';
 import Menu from '@/features/profile/menu';
 import mockUser from '@/features/profile/mock';
-import { IMenuData } from '@/features/profile/profile.interface';
+import useForm from '@/hook/useValidate';
+import Input from '@/components/input/input';
+import IMenuEditProfileData from './profile.interface';
+import { ValidationProps } from '@/features/validation/validator';
+import { MenuState, MenuType } from '@/features/profile/menu/menu.interface';
 import './profile.scss';
 
-const userMenuData: IMenuData[] = [
-  { key: 'Имя', value: mockUser.name },
-  { key: 'Фамилия', value: mockUser.secondName },
-  { key: 'Логин', value: mockUser.login },
-  { key: 'Телефон', value: mockUser.phone },
-  { key: 'Имя в чате', value: mockUser.chatName },
+const userMenuData: IMenuEditProfileData[] = [
+  { key: 'Имя', name: 'first_name', value: mockUser.name },
+  { key: 'Фамилия', name: 'second_name', value: mockUser.secondName },
+  { key: 'Логин', name: 'login', value: mockUser.login },
+  { key: 'Телефон', name: 'phone', value: mockUser.phone },
+  { key: 'Имя в чате', name: 'display_name', value: mockUser.chatName },
 ];
 
+const initValues = userMenuData.reduce((acc, { name, value }) => {
+  acc[name] = value;
+
+  return acc;
+}, {} as Record<string, string>);
+
+const validationSchema: Record<string, ValidationProps> = {
+  first_name: {
+    required: true,
+  },
+  second_name: {
+    required: true,
+  },
+  login: {
+    required: true,
+  },
+  phone: {
+    required: true,
+    isPhone: true,
+  },
+  display_name: {
+    required: true,
+  },
+};
+
 const EditProfile: FC = () => {
+  const {
+    values,
+    hasError,
+    onChangeForm,
+    getFieldProps,
+    getFieldError,
+    onBlurInput,
+  } = useForm({ initValues, validationSchema });
+
+  const navigate = useNavigate();
+
+  const onSubmitForm = async (evt: FormEvent) => {
+    evt.preventDefault();
+    console.log(values);
+
+    navigate('/profile');
+  };
+
   return (
     <div className="profile-edit">
-      <Avatar>{`${mockUser.name} ${mockUser.secondName}`}</Avatar>
+      <Avatar editable>{`${mockUser.name} ${mockUser.secondName}`}</Avatar>
 
       <div className="profile-edit__content">
-        <Menu className="profile-edit__menu" title="Сохранить">
-          <div className="profile-edit__menu-content">
-            <dl>
-              {userMenuData.map(({ key, value }) => (
+        <form
+          onChange={onChangeForm}
+          onSubmit={onSubmitForm}
+          autoComplete="off">
+          <Menu
+            className="profile-edit__menu"
+            title="Сохранить"
+            state={hasError ? MenuState.ERROR : MenuState.SUCCESS}
+            type={MenuType.SUBMIT}>
+            <div className="profile-edit__menu-content">
+              {userMenuData.map(({ key, name }) => (
                 <React.Fragment key={key}>
-                  <dt className="profile__label">{key}</dt>
-                  <dd className="profile__value">{value}</dd>
+                  <label className="profile-edit__label">
+                    {key}
+                    <Input
+                      {...getFieldProps(name)}
+                      error={getFieldError(name)}
+                      onBlur={onBlurInput}
+                    />
+                  </label>
                 </React.Fragment>
               ))}
-            </dl>
-          </div>
-        </Menu>
+            </div>
+          </Menu>
+        </form>
       </div>
     </div>
   );
