a2a23e952b73df51b609716b257b4858beb49d9a
dmitry sumanov <dmitryra27@gmail.com>
2023-04-14 00:02:40 +0300

    Какую задачу решаем Добавление логики игры Было реализовано:
    добавлена логика движения танков противника в рандомном направлении после столкновения с препятствием

--- packages/client/src/features/game/traits/enemy.ts
+++ packages/client/src/features/game/traits/enemy.ts
@@ -1,21 +1,51 @@
-import { SIDES } from '../constants';
-import { Entity } from '../entity';
-import { Go } from './go';
-import { Trait } from './trait';
+import {SIDES} from '../constants';
+import {Entity} from '../entity';
+import {Go} from './go';
+import {Trait} from './trait';
 
 class Enemy extends Trait {
   constructor() {
     super('enemy');
   }
 
+  arrSides = [SIDES.LEFT, SIDES.RIGHT, SIDES.BOTTOM,SIDES.TOP]
+
+  random(arr:string[]):string{
+    const rand = Math.floor(Math.random() * arr.length);
+    return arr[rand]
+  }
+
+  excludeDirection(arr:string[], direction:string):string[]{
+    arr = arr.filter(function( obj:string ) {
+      return obj !== direction;
+    });
+    return arr
+  }
+
   obstruct(entity: Entity, side: SIDES): void {
+
     const go = entity.getTrait('go') as Go;
-    if (side === SIDES.LEFT || side === SIDES.RIGHT) {
-      go.directionX = -go.directionX;
-      go.side = side === SIDES.LEFT ? SIDES.RIGHT : SIDES.LEFT;
-    } else {
-      go.directionY = -go.directionY;
-      go.side = side === SIDES.TOP ? SIDES.BOTTOM : SIDES.TOP;
+    const arrDirections = this.excludeDirection(this.arrSides, side);
+    //console.log('side = ', side,'arrDirections = ',arrDirections)
+    const newSide = this.random(arrDirections);
+    console.log('side= ', side,' newSide = ', newSide);
+    go.side = SIDES.LEFT;
+
+    if (newSide===SIDES.LEFT){
+      go.directionX = -1
+      go.side = SIDES.LEFT;
+    }
+    else if(newSide===SIDES.RIGHT){
+      go.directionX = +1
+      go.side = SIDES.RIGHT;
+    }
+    else if(newSide===SIDES.TOP){
+      go.directionY = -1
+      go.side = SIDES.TOP;
+    }
+    else {
+      go.directionY = +1
+      go.side = SIDES.BOTTOM;
     }
   }
 }
