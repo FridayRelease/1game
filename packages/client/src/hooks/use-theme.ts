f08f319a72938f0818d3dee14eefbe9e6943df02
Sergey Pudov <pellexa@gmail.com>
2023-04-14 16:13:51 +0300

    Теперь можно добавлять более двух тем.

--- packages/client/src/hooks/use-theme.ts
+++ packages/client/src/hooks/use-theme.ts
@@ -1,19 +1,19 @@
 import { themeActions } from '@/store/slices/theme-slice';
 import { store } from '@/store/store';
-import { getLocalStorage, setLocalStorage } from '@/utils/localStorage';
+import { getLocalStorage, setLocalStorage } from '@/utils/local-storage';
 import { useEffect } from 'react';
 
 const useTheme = () => {
+  let theme = getLocalStorage('theme');
+
   useEffect(() => {
-    let theme = getLocalStorage('theme');
     if (!theme.value) {
-      const stateTheme = store.getState().theme;
-      setLocalStorage('theme', JSON.stringify({ value: stateTheme.value }));
-      theme = stateTheme;
+      theme = store.getState().theme;
+      setLocalStorage('theme', JSON.stringify({ value: theme.value }));
     }
 
     store.dispatch(themeActions.setTheme(theme.value));
-  }, []);
+  }, [theme.value]);
 };
 
 export default useTheme;
