b8b2147229ad360f1a0bc4a04bc9d801818a2b87
dmitry sumanov <dmitryra27@gmail.com>
2023-04-18 15:59:03 +0300

    Исправлены замечания ревьюира

--- packages/client/src/app.tsx
+++ packages/client/src/app.tsx
@@ -20,7 +20,7 @@ function App() {
   useEffect(() => {
     if (window.Worker) {
       const fetchServerData = async () => {
-        const url = `http://localhost:3001`;
+        const url = `http://localhost:${__SERVER_PORT__}`;
         const response = await fetch(url);
         const data = await response.json();
         console.log(data);
