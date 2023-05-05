02742f2070ec0641e066e56ac2182db832e50892
Sergey Pudov <pellexa@gmail.com>
2023-05-05 15:19:50 +0300

    Vite конфиги.

--- packages/client/ssr.config.ts
+++ packages/client/ssr.config.ts
@@ -3,18 +3,18 @@ import { fileURLToPath, URL } from 'url';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import dotenv from 'dotenv';
+import * as path from 'path';
 dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: Number(process.env.CLIENT_PORT) || 3000,
-  },
-  define: {
-    __SERVER_PORT__: process.env.SERVER_PORT || 3001,
-  },
   build: {
-    outDir: 'dist/client',
+    lib: {
+      entry: path.resolve(__dirname, 'ssr.tsx'),
+      name: 'Client',
+      formats: ['cjs'],
+    },
+    outDir: 'dist/ssr',
   },
   plugins: [
     react(),
