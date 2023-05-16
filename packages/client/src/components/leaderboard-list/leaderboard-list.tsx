84cb4c800209d4cd0f0ffe59f083f68f98dba7f4
dmitry sumanov <dmitryra27@gmail.com>
2023-05-16 18:25:19 +0300

    Исправление ошибки в модуле - leaderboard-list.tsx

--- packages/client/src/components/leaderboard-list/leaderboard-list.tsx
+++ packages/client/src/components/leaderboard-list/leaderboard-list.tsx
@@ -1,4 +1,3 @@
-
 import Position from '../position';
 import './leaderboard-list.scss';
 import {LeaderboardSelectors} from "@/store/slices/leaderboard-slice";
@@ -6,24 +5,24 @@ import {useSelector} from "react-redux";
 
 const LeaderboardList = () => {
 
-  const leaderboardListData = useSelector(LeaderboardSelectors.all);
-  console.log('leaderboardListData = ', leaderboardListData)
-  const copyArray = [...leaderboardListData];
-  const sortedArray = copyArray.sort((a, b) => (Number(b.score) - Number(a.score)));
+    const leaderboardListData = useSelector(LeaderboardSelectors.all);
+    console.log('leaderboardListData = ', leaderboardListData)
+    const copyArray = [...leaderboardListData.leaderboard];
+    const sortedArray = copyArray.sort((a, b) => (Number(b.score) - Number(a.score)));
 
-  const itemList = sortedArray.map((item, index) => {
-    const position = index + 1;
+    const itemList = sortedArray.map((item, index) => {
+        const position = index + 1;
 
-    return (
-      <li className="leaderboard__row" key={position}>
-        <Position className="leaderboard__position" position={position} />
-        <div className="leaderboard__name">{item.name}</div>
-        <div className="leaderboard__score">{item.score}</div>
-      </li>
-    );
-  });
+        return (
+            <li className="leaderboard__row" key={position}>
+                <Position className="leaderboard__position" position={position}/>
+                <div className="leaderboard__name">{item.name}</div>
+                <div className="leaderboard__score">{item.score}</div>
+            </li>
+        );
+    });
 
-  return <ul className="leaderboard__data">{itemList}</ul>;
+    return <ul className="leaderboard__data">{itemList}</ul>;
 };
 
 export default LeaderboardList;
