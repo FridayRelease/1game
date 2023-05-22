85afa77034cdec3069e3cc601a77b27a04d01e00
Timerbayev Aynur <timerbayevaf@yandex.ru>
2023-05-22 20:04:07 +0300

    fix: правка отправки запроса

--- packages/client/src/controllers/lider-controller.ts
+++ packages/client/src/controllers/lider-controller.ts
@@ -1,38 +1,38 @@
-import {liderApi} from '@/api';
-import {ILeaderboardAddUser, IQuery} from "@/api/types";
-import {AnyAction, Dispatch} from "@reduxjs/toolkit";
-import {IErrorState} from "@/store/slices/error-slice";
-import Leaderboard from "@/pages/leaderboard";
-import {LeaderboardActions, ILeader, ILeaderboardState} from "@/store/slices/leaderboard-slice";
-import {AppDispatch} from "@/store/store";
+import { liderApi } from '@/api';
+import { ILeaderboardAddUser, IQuery } from '@/api/types';
+import { AnyAction, Dispatch } from '@reduxjs/toolkit';
+import { IErrorState } from '@/store/slices/error-slice';
+import Leaderboard from '@/pages/leaderboard';
+import { LeaderboardActions, ILeader, ILeaderboardState } from '@/store/slices/leaderboard-slice';
+import { AppDispatch } from '@/store/store';
 
 //добавление пользователя с его очками на сервер
 export const addUserDatasToServer = async (info: ILeaderboardAddUser) => {
-  const {data} = await liderApi.add_user_to_leaderboard(info);
-  console.log('info addUserDatasToServer=', info)
+  const { data } = await liderApi.add_user_to_leaderboard(info);
+  console.log('info addUserDatasToServer=', info);
 
-  if (data === 'Ok') {
+  if (data === 'OK') {
     return data;
   }
-  console.log('data is not OK = ', data)
+  console.log('data is not OK = ', data);
 
   throw new Error('Произошла ошибка при добавлении пользователя');
 };
 //-------------------
 //const gameName =  '1game'
-const query: IQuery = {// сортировка score, 1 страница на 10 записей
-  ratingFieldName: "score",
+const query: IQuery = {
+  // сортировка score, 1 страница на 10 записей
+  ratingFieldName: 'score',
   cursor: 0,
   limit: 10,
-}
+};
 // получаем с сервера данные игрока и его очки и записываем в Store
 // реально получаем данные только одного игрока
 export const getLeaderboardDatas = async (query: IQuery) => {
-  console.log('Start in getLeaderboardDatas')
-  const {data} = await liderApi.get_team_leaderboard(query);
-  console.log('END in getLeaderboardDatas')
+  console.log('Start in getLeaderboardDatas');
+  const { data } = await liderApi.get_team_leaderboard(query);
+  console.log('END in getLeaderboardDatas');
   return data;
-
 
   //throw new Error('Произошла ошибка при получении данных пользователя и его очков с сервера ');
 };
@@ -40,7 +40,4 @@ export const getLeaderboardDatas = async (query: IQuery) => {
 // запиcь данных в Store
 export const setUserDatasToStore = (data: ILeader, dispatch: Dispatch<AnyAction>) => {
   dispatch(LeaderboardActions.setLeaderboard(data));
-}
-
-
-
+};
--- packages/client/src/pages/proba/proba.tsx
+++ packages/client/src/pages/proba/proba.tsx
@@ -1,10 +1,10 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import withLayoutMain from '@/layout/layoutMain/layoutMain';
-import {ILeaderboardAddUser, IQuery} from "@/api/types";
-import {addUserDatasToServer, getLeaderboardDatas, setUserDatasToStore} from "@/controllers/lider-controller";
-import {useDispatch, useSelector} from 'react-redux';
-import {LeaderboardSelectors} from "@/store/slices/leaderboard-slice";
-import {Link} from "react-router-dom";
+import { ILeaderboardAddUser, IQuery } from '@/api/types';
+import { addUserDatasToServer, getLeaderboardDatas, setUserDatasToStore } from '@/controllers/lider-controller';
+import { useDispatch, useSelector } from 'react-redux';
+import { LeaderboardSelectors } from '@/store/slices/leaderboard-slice';
+import { Link } from 'react-router-dom';
 
 /**
  * Страница proba
@@ -12,73 +12,81 @@ import {Link} from "react-router-dom";
  * @category page
  */
 const Proba: FC = () => {
+  const dispatch = useDispatch();
+  const data = { name: 'SEM', score: 500 };
+  const info: ILeaderboardAddUser = {
+    data: data,
+    ratingFieldName: 'score',
+    teamName: '1game',
+  };
 
-    const dispatch = useDispatch();
-    const data = {name:"SEM", "score":500}
-    const info:ILeaderboardAddUser = {
-        "data": data,
-        "ratingFieldName": "score",
-        "teamName": "1game"
-    }
-    console.log('file use-start data {name, score} to server', data)
-    let leaderboardListData = useSelector(LeaderboardSelectors.all);
-//----сравнение и запись в локальный Store--------
-    if (data.name!==undefined && data.score!==undefined){
-        console.log('Проверяет пользователя и его очки = ', data)
-        const length = leaderboardListData.leaderboard.length
-        for (let i=0; i<length;i++){
-            if (leaderboardListData.leaderboard[i].name===data.name){
-                console.log('Пользователь уже играл, проверяем его лучший результат')
-                if (leaderboardListData.leaderboard[i].score < data.score){
-                    setUserDatasToStore(data,dispatch);//запись в Store
-                    console.log('Очки игрока в Стор записали')
-                    break;
-                } else {
-                    console.log('Данные по очкам Игрока проверили, записывать не стали')
-                    break;
-                }
-            }
-            if (leaderboardListData.leaderboard[i].name!==data.name && i==length-1){
-                setUserDatasToStore(data,dispatch);//запись в Store
-                console.log('Нового игрока и его очки в Стор записали')
-            }
+  console.log('file use-start data {name, score} to server', data);
+  let leaderboardListData = useSelector(LeaderboardSelectors.all);
+  //----сравнение и запись в локальный Store--------
+  if (data.name !== undefined && data.score !== undefined) {
+    console.log('Проверяет пользователя и его очки = ', data);
+    const length = leaderboardListData.leaderboard.length;
+    for (let i = 0; i < length; i++) {
+      if (leaderboardListData.leaderboard[i].name === data.name) {
+        console.log('Пользователь уже играл, проверяем его лучший результат');
+        if (leaderboardListData.leaderboard[i].score < data.score) {
+          setUserDatasToStore(data, dispatch); //запись в Store
+          console.log('Очки игрока в Стор записали');
+          break;
+        } else {
+          console.log('Данные по очкам Игрока проверили, записывать не стали');
+          break;
         }
-     }
-//------получение информации из стора и сортировка перед выводом на экран----
-    leaderboardListData = useSelector(LeaderboardSelectors.all);
-    console.log('leaderboardListData = ', leaderboardListData)
-    const copyArray = [...leaderboardListData.leaderboard];
-    const sortedArray = copyArray.sort((a, b) => (Number(b.score) - Number(a.score)));
-    console.log('sortedArray = ', sortedArray)
-    //--------
-
-    //-------------------
-    //const gameName =  '1game'
-    const query:IQuery = {// сортировка score, 1 страница на 10 записей
-        ratingFieldName: "score",
-        cursor: 0,
-        limit:10,
+      }
+      if (leaderboardListData.leaderboard[i].name !== data.name && i == length - 1) {
+        setUserDatasToStore(data, dispatch); //запись в Store
+        console.log('Нового игрока и его очки в Стор записали');
+      }
     }
-    // получаем с сервера данные игрока и его очки и записываем в Store
-        console.log('start getLeaderboardDatas')
-      const result = getLeaderboardDatas( query)// от тут валится приложение,
-    console.log('end getLeaderboardDatas')
+  }
+  //------получение информации из стора и сортировка перед выводом на экран----
+  leaderboardListData = useSelector(LeaderboardSelectors.all);
+  console.log('leaderboardListData = ', leaderboardListData);
+  const copyArray = [...leaderboardListData.leaderboard];
+  const sortedArray = copyArray.sort((a, b) => Number(b.score) - Number(a.score));
+  console.log('sortedArray = ', sortedArray);
+  //--------
 
+  //-------------------
+  //const gameName =  '1game'
+  const query: IQuery = {
+    // сортировка score, 1 страница на 10 записей
+    ratingFieldName: 'score',
+    cursor: 0,
+    limit: 10,
+  };
+  // получаем с сервера данные игрока и его очки и записываем в Store
+  console.log('start getLeaderboardDatas');
+  console.log('end getLeaderboardDatas');
 
-    //--------------
-    try{
-        //addUserDatasToServer(info);// Запись на Сервер - // и вот тут валится приложение, поскольку обращается к локальному адресу
-        //console.log('Данные игрока и очки на Сервер записали')
-    }catch (e) {
+  useEffect(() => {
+    (async () => {
+      //--------------
+      try {
+        const result = await getLeaderboardDatas(query); // от тут валится приложение,
+        const res2 = await addUserDatasToServer(info); // Запись на Сервер - // и вот тут валится приложение, поскольку обращается к локальному адресу
+        console.log('Данные игрока и очки на Сервер записали');
+
+        console.warn(result, res2);
+      } catch (e) {
         //console.log('Ошибка записи на сервер результатов Игрока', e)
-    }
- //----------
-    return (
-        <div className="leaderboard">
-           <h1>Proba page</h1>
-            <Link to='/leaderboard'> Перейти в Leader </Link>
-        </div>
-    );
+      }
+    })();
+
+    //----------
+  }, []);
+
+  return (
+    <div className="leaderboard">
+      <h1>Proba page</h1>
+      <Link to="/leaderboard"> Перейти в Leader </Link>
+    </div>
+  );
 };
 
 Proba.displayName = 'Leaderboard';
