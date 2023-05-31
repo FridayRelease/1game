6da9d452ee02321e890854016f9d93f3e21b5be3
dmitry sumanov <dmitryra27@gmail.com>
2023-05-31 21:31:30 +0300

    commit - работающий после восстановления

--- packages/client/src/api/forum.ts
+++ packages/client/src/api/forum.ts
@@ -17,20 +17,20 @@ export class ForumApi {
 
   //Создать Топик и работа с ним
   topicCreate = async (data: ITopicCreate) => {
-    return await this.http.post('/api/v1/topics', data);
+    return await this.http.post('/topics', data);
   };
   getTopicsAll = async () => {//topicGet - string
-    return await this.http.get(`/api/v1/topics/`);
+    return await this.http.get(`/topics/`);
   };
   getTopicById = async (id: number) => {//topicRead - string
-    return await this.http.get(`/api/v1/topics/${id}`);
+    return await this.http.get(`/topics/${id}`);
   };
   updateTopic = async (id: number, data:string) => {//topicUpdate
-    return await this.http.put(`/api/v1/topics/${id}`, data);
+    return await this.http.put(`/topics/${id}`, data);
   };
 
   deleteTopic = async (id: number) => {//topicDelete
-    return await this.http.delete(`/api/v1/topics/${id}`);
+    return await this.http.delete(`/topics/${id}`);
   };
 
   /*
@@ -42,29 +42,29 @@ export class ForumApi {
   * */
   //Создать Комментарий и работа с ним
   commentCreate = async (data: ICommentCreate) => {
-    return await this.http.post('/api/v1/comments', data);
+    return await this.http.post('/comments', data);
   };
   getCommentsAll = async () => {//Get all comments
-    return await this.http.get(`/api/v1/comments/`);
+    return await this.http.get(`/comments/`);
   };
   getCommentsById = async (id: number) => {//commentRead by Id
-    return await this.http.get(`/api/v1/comments/${id}`);
+    return await this.http.get(`/comments/${id}`);
   };
   getCommentsByIdTopic = async (id: number) => {//commentRead by Id Topic
-    return await this.http.get(`/api/v1/comments/topic/${id}`);
+    return await this.http.get(`/comments/topic/${id}`);
   };
   updateComment = async (id: number, data:string) => {//Update comment
-    return await this.http.put(`/api/v1/comments/${id}`, data);
+    return await this.http.put(`/comments/${id}`, data);
   };
 
   deleteComment = async (id: number) => {//delete comment
-    return await this.http.delete(`/api/v1/comments/${id}`);
+    return await this.http.delete(`/comments/${id}`);
   };
   /*
 * users - /api/v1/users
 * */
   getUsers = async () => {
-    return await this.http.get(`/api/v1/users/`);
+    return await this.http.get(`/users/`);
   };
 
 }
