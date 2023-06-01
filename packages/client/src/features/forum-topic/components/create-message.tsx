bea0ffcd98c3c36fddb242f81f800338f48156e6
dmitry sumanov <dmitryra27@gmail.com>
2023-06-01 14:00:02 +0300

    commit - исправления 1

--- packages/client/src/features/forum-topic/components/create-message.tsx
+++ packages/client/src/features/forum-topic/components/create-message.tsx
@@ -1,52 +1,60 @@
 import './topic-comment.scss';
 import React, {ChangeEvent, ChangeEventHandler, FormEvent, useState} from "react";
-import {ICommentCreate,} from "@/api/types";
+import {ICommentCreate, ITopicCreate,} from "@/api/types";
 import {useDispatch, useSelector} from "react-redux";
 import {userSelectors} from "@/features/authentication";
 import {addTopicToServer} from "@/controllers/forum-topic-controller";
 import {addCommentToServer} from "@/controllers/forum-comments-controller";
 import {ForumSelectors} from "@/store/slices/forum-slice";
+
 /**
  Компонент Страницы одного Топика с Сообщениями
  @category page
  */
 
 export const CreateMessage = () => {
-        const [text, setText] = useState('');
-        const user = useSelector(userSelectors.user)
-        //const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-        const today = new Date();
-        const data:ICommentCreate = {
-            message:text,
-            user_id:user.info!.id,
-            comment_id:useSelector(ForumSelectors.commentId),
-            topic_id:useSelector(ForumSelectors.id),
-            created_at:today.toLocaleDateString("en-US"),
-            updated_at:today.toLocaleDateString("en-US"),
+    const [text, setText] = useState('');
+    const user = useSelector(userSelectors.user)
+    //const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    const today = new Date();
+    const data: ICommentCreate = {
+        message: text,
+        user_id: user.info!.id,
+        comment_id: useSelector(ForumSelectors.commentId),
+        topic_id: useSelector(ForumSelectors.id),
+        created_at: today.toLocaleDateString("en-US"),
+        updated_at: today.toLocaleDateString("en-US"),
 
-        }
-        const dispatch = useDispatch()
-        const onSubmit  = (e:FormEvent) =>  {
-            e.preventDefault();
-            console.log('datas for create new comment = ', data)
-            const result = addCommentToServer(data);
-            console.log('result of add comment to Server', result)
-            //dispatch(ForumActions.setTopicsFromServerToStore(data));
-            //
-        };
-    function update(event:ChangeEvent<HTMLInputElement>) {
+    }
+    const dispatch = useDispatch()
+    const onSubmit = (e: FormEvent, data: ICommentCreate) => {//
+        e.preventDefault();
+        console.log('datas for create new topic = ', data)
+        addToServer(data);
+    };
+
+    async function addToServer(data: ICommentCreate) {
+        await addCommentToServer(data).then(res => console.log('res = ', res))
+    }
+
+    function update(event: ChangeEvent<HTMLInputElement>) {
         setText(event.target.value)
     }
-        return(
-            <div className={'create-comment'}>
-                <form onSubmit={onSubmit}>
-                    <div>
-                        <label htmlFor="InputName1">Написать сообщение:</label>
-                        <input id="InputName1" type="text" value={text} onChange={event => update(event)} />
-                    </div>
-                    <button type="submit">Отправить</button>
-                </form>
-            </div>
 
-        );
+    function update1(event: ChangeEvent<HTMLInputElement>) {
+        setText(event.target.value)
     }
+
+    return (
+        <div className={'create-comment'}>
+            <form onSubmit={onSubmit}>
+                <div>
+                    <label htmlFor="InputName1">Написать сообщение:</label>
+                    <input id="InputName1" type="text" value={text} onChange={event => update1(event)}/>
+                </div>
+                <button type="submit">Отправить</button>
+            </form>
+        </div>
+
+    );
+}
