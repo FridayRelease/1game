1c0cde993b5aeaff6a7688d7b51824234d7a4496
Timerbayev Aynur <timerbayevaf@yandex.ru>
2023-06-03 00:26:33 +0300

    fix: добавление sw

---
+++ packages/client/src/constant/game-path.ts
@@ -0,0 +1,27 @@
+// необходимые urls для игры
+const paths = [
+  `${import.meta.env.VITE_ASSETS_API}levels/1-1.json`,
+  `${import.meta.env.VITE_ASSETS_API}levels/1-2.json`,
+  `${import.meta.env.VITE_ASSETS_API}levels/1-3.json`,
+  `${import.meta.env.VITE_ASSETS_API}img/fonts.png`,
+  `${import.meta.env.VITE_ASSETS_API}sprites/bullet.json`,
+  `${import.meta.env.VITE_ASSETS_API}sprites/tank.json`,
+  `${import.meta.env.VITE_ASSETS_API}sounds/tank.json`,
+  `${import.meta.env.VITE_ASSETS_API}sprites/enemy.json`,
+  `${import.meta.env.VITE_ASSETS_API}sprites/eagle.json`,
+  `${import.meta.env.VITE_ASSETS_API}img/tanks.png`,
+  `${import.meta.env.VITE_ASSETS_API}img/bullet.png`,
+  `${import.meta.env.VITE_ASSETS_API}img/tiles.png`,
+  `${import.meta.env.VITE_ASSETS_API}sprites/overworld.json`,
+  `${import.meta.env.VITE_ASSETS_API}img/ground.png`,
+  `${import.meta.env.VITE_ASSETS_API}sounds/overworld.json`,
+  `${import.meta.env.VITE_ASSETS_API}sprites/patterns/overworld-pattern.json`,
+  `${import.meta.env.VITE_ASSETS_API}audio/levelstarting.ogg`,
+  `${import.meta.env.VITE_ASSETS_API}audio/shoot.ogg`,
+  `${import.meta.env.VITE_ASSETS_API}audio/fexplosion.ogg`,
+  `${import.meta.env.VITE_ASSETS_API}audio/moving.ogg`,
+  `${import.meta.env.VITE_ASSETS_API}audio/tank_idle.wav`,
+  `${import.meta.env.VITE_ASSETS_API}audio/bonus.ogg`,
+];
+
+export { paths };
