f40bcbd091d5f92038acdf08cff52ca83a077eb4
Timerbayev Aynur <timerbayevaf@yandex.ru>
2023-06-05 16:30:10 +0300

    fix: добавление error-boundary и .env

--- packages/client/src/api/index.ts
+++ packages/client/src/api/index.ts
@@ -2,8 +2,11 @@ import { GameApi } from './game';
 import { UserApi } from './user';
 import { LeaderApi } from '@/api/leader';
 
+const assetsApi =
+  process.env.NODE_ENV === 'development' ? import.meta.env.VITE_DEV_ASSETS_API : import.meta.env.VITE_BASE_API;
+
 const userApi = new UserApi(import.meta.env.VITE_BASE_API || '/api/v2/');
-const gameApi = new GameApi(import.meta.env.VITE_ASSETS_API);
+const gameApi = new GameApi(assetsApi);
 const leaderApi = new LeaderApi(import.meta.env.VITE_BASE_API || '/');
 
 export { userApi, gameApi, leaderApi };
--- packages/client/src/features/error-boundary/ErrorBoundary.tsx
+++ packages/client/src/features/error-boundary/ErrorBoundary.tsx
@@ -6,25 +6,38 @@ interface Props {
 
 interface State {
   hasError: boolean;
+  error: Error | null;
+  errorInfo: ErrorInfo | null;
 }
 
 class ErrorBoundary extends Component<Props, State> {
   public state: State = {
     hasError: false,
+    error: null,
+    errorInfo: null,
   };
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   public static getDerivedStateFromError(_: Error): State {
-    return { hasError: true };
+    return { hasError: true, error: null, errorInfo: null };
   }
 
   public componentDidCatch(error: Error, errorInfo: ErrorInfo) {
-    console.error('Uncaught error:', error, errorInfo);
+    this.setState({ hasError: true, error, errorInfo });
   }
 
   public render() {
     if (this.state.hasError) {
-      return <h1>Sorry.. there was an error</h1>;
+      return (
+        <>
+          <h2>Что-то пошло не так.</h2>
+          <details style={{ whiteSpace: 'pre-wrap' }}>
+            {this.state.error && this.state.error.toString()}
+            <br />
+            {this.state.errorInfo?.componentStack}
+          </details>
+        </>
+      );
     }
 
     return this.props.children;
