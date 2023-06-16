d6b8fb0abee3a89d6b855894a8cc9c1c9bdb92e4
Sergey Pudov <pellexa@gmail.com>
2023-06-16 14:10:03 +0300

    поправил выход из профиля

--- packages/client/src/pages/profile/profile.tsx
+++ packages/client/src/pages/profile/profile.tsx
@@ -2,18 +2,15 @@ import { FC } from 'react';
 import Profile from '@/components/profile';
 import withLayoutMain from '@/layout/layoutMain/layoutMain';
 import './profile.scss';
-import { RequireAuth } from '@/features/authentication';
 
 const ProfilePage: FC = () => {
   return (
-    <RequireAuth>
-      <div className="profile-page">
-        <h1 className="visually-hidden">Профиль пользователя</h1>
-        <main className="container">
-          <Profile />
-        </main>
-      </div>
-    </RequireAuth>
+    <div className="profile-page">
+      <h1 className="visually-hidden">Профиль пользователя</h1>
+      <main className="container">
+        <Profile />
+      </main>
+    </div>
   );
 };
 
