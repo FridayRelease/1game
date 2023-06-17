1299b339ad3b60d371db6c518b65551b603bf007
avazhov <avazhov@cinimex.com>
2023-06-17 12:20:35 +0300

    Обновил типы

--- packages/server/utils/data.ts
+++ packages/server/utils/data.ts
@@ -1,5 +1,5 @@
 import type { IResponseWithPaginate } from 'pagination';
-import type { Reaction } from '../src/models/reaction';
+import type { ExtendReaction, ObjectReaction } from 'reactions';
 
 const paginateResponse = <T>(count: number, rows: Array<T>, offset = 0, limit = 0): IResponseWithPaginate<T> => {
   return {
@@ -10,10 +10,8 @@ const paginateResponse = <T>(count: number, rows: Array<T>, offset = 0, limit = 
   };
 };
 
-type ExtendReaction = Omit<Reaction, 'user'> & { user_id: number }
-
 const groupingReaction = (reactions: Array<ExtendReaction>, user_id?: number) => {
-  const newObject: Record<string, any> = {}
+  const newObject: Record<string, ObjectReaction> = {}
 
   for (const reaction of reactions) {
     if (newObject[reaction.reaction.name]) {
