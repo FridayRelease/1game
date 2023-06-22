d8ff025b29287636122023f442450f4894ac5bf1
Sergey Pudov <pellexa@gmail.com>
2023-06-22 14:28:26 +0300

    правка src/api/index.ts

--- packages/client/src/api/index.ts
+++ packages/client/src/api/index.ts
@@ -8,7 +8,7 @@ import { OAuth } from './o-auth';
 const assetsApi =
   process.env.NODE_ENV === 'development' ? import.meta.env.VITE_DEV_ASSETS_API : import.meta.env.VITE_ASSETS_API;
 
-const userApi = new UserApi(import.meta.env.VITE_BASE_API);
+const userApi = new UserApi(import.meta.env.VITE_BASE_API || '/api/v2/');
 const gameApi = new GameApi(assetsApi);
 const leaderApi = new LeaderApi(import.meta.env.VITE_BASE_API || '/api/v2/');
 const oauthApi = new OAuth(import.meta.env.VITE_BASE_API || '/api/v2/');
